687787b4173b5ddb371be9d2cd3243dbfbeb7717
cjmoro <scjmoro@gmail.com>
2020-03-31 00:45:29 -0700

    Add output string

--- course-starting-project/app.ts
+++ course-starting-project/app.ts
@@ -1,12 +1,12 @@
-function add(n1: number, n2: number, showResult: boolean) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
 	// if (typeof n1 !== "number" || typeof n2 !== "number") {
 	// 	throw new Error("Incorrect input!");
 	// }
 	if (showResult) {
-		console.log(n1 + n2);
+		console.log(phrase + n1 + n2);
 	} else {
 		return n1 + n2;
 	}
 }
 
-const result = add(5, 8, true);
+const result = add(5, 8, true, "Result is: ");
