a21ac3f1177b6af92be9195c0aa972875b204432
cjmoro <scjmoro@gmail.com>
2020-04-02 23:19:50 -0700

    Add private and public modifiers

--- course-starting-project/classes-and-interfaces.ts
+++ course-starting-project/classes-and-interfaces.ts
@@ -1,19 +1,44 @@
 class Department {
+	private employees: string[] = [];
+
+	// note, can add public but it's a default so redundant
 	name: string;
 
-	constructor(n: string) {
+	// specifying modifiers for constructor properties becomes redundant
+	// private id: string;
+	// instead, can specify in constructor
+	// however, public modifiers also need to be specified
+	constructor(private id: string, n: string, public desc: string) {
+		this.id = id;
 		this.name = n;
+		this.name;
 	}
 
 	// specify this context for typescript
 	describe(this: Department) {
-		console.log("Department: " + this.name);
+		console.log("Department: " + this.name + this.id + this.desc);
+	}
+
+	addEmployee(employee: string) {
+		this.employees.push(employee);
+	}
+	printEmployeeInformation() {
+		console.log(this.employees.length);
 	}
 }
 
-const accounting = new Department("Accounting");
+const accounting = new Department("tests", "Accounting", "some description");
 
 accounting.describe();
+accounting.addEmployee("CJ");
+accounting.addEmployee("Batman");
+
+// works but will cause confusion in the code
+// to avoid, add private to employees array property
+// then it will throw an error
+// accounting.employees[1] = "Superman";
+
+accounting.printEmployeeInformation();
 
 const accountingCopy = { describe: accounting.describe };
 
