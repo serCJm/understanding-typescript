c3a604e7a202465a4c9a7e90514e9c3f6e563dbc
cjmoro <scjmoro@gmail.com>
2020-04-04 12:59:30 -0700

    Add utility types

--- course-starting-project/generics.ts
+++ course-starting-project/generics.ts
@@ -119,3 +119,28 @@ console.log(objStorage.getItems());
 // 		return [...this.data];
 // 	}
 // }
+
+// GENERIC UTILITY TYPES
+
+// Partial
+interface CourseGoal {
+	title: string;
+	description: string;
+	completeUntil: Date;
+}
+
+function createCourseGoal(
+	title: string,
+	description: string,
+	date: Date
+): CourseGoal {
+	let courseGoal: Partial<CourseGoal> = {};
+	courseGoal.title = title;
+	courseGoal.description = description;
+	courseGoal.completeUntil = date;
+	return courseGoal as CourseGoal;
+}
+
+// Readonly
+const readonlyNames: Readonly<string[]> = ["Batman", "Robin"];
+// readonlyNames.push("Joker"); // error! can't modify readonlyNames
