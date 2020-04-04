a21371a7e35aff3b736a5258bd282e0bf03ab095
cjmoro <scjmoro@gmail.com>
2020-04-03 18:37:10 -0700

    Add optional properties and param

--- course-starting-project/interfaces.ts
+++ course-starting-project/interfaces.ts
@@ -6,10 +6,13 @@
 // interface can be used as a contract to which a class needs to adhere to
 // used to share and force functionality amongst classes regarding the structure and features they should have.
 // unlike abstract classes, interfaces have no implementation details
+// note, interfaces do NOT compile to the output file
 interface Greetable {
 	name: string;
 	age: number;
 	readonly nickname: string;
+	// optional property
+	salary?: number;
 
 	greet(phrase: string): void;
 }
@@ -30,14 +33,18 @@ user1.greet("My name is ");
 
 // note, can implement more than one interface by separating with a comma
 class Person implements Greetable {
+	salary?: number;
+
 	constructor(
 		public name: string,
 		public age: number,
-		public nickname: string
+		public nickname: string,
+		s?: number
 	) {
 		this.name = name;
 		this.age = age;
 		this.nickname = nickname;
+		this.salary = s;
 	}
 	greet(phrase: string) {
 		console.log(phrase + this.name + " Bond." + this.age + this.nickname);
