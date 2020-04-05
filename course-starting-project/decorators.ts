f1f8882cdf2f2f2617c41023dfd677da5db12b03
cjmoro <scjmoro@gmail.com>
2020-04-04 17:34:59 -0700

    Add property, method, and parameter decorators

--- course-starting-project/decorators.ts
+++ course-starting-project/decorators.ts
@@ -3,6 +3,7 @@
 // in order to be able to use decorators
 
 // decorator is a function you apply to a class in a certain way
+// note, property decorators execute during class declaration only!!!
 
 function Logger(target: Function) {
 	console.log("Logging...");
@@ -51,5 +52,65 @@ class PersonDecorator2 {
 
 	constructor() {
 		console.log("Creating person project...");
+	}
+}
+
+// PROPERTY DECORATORS
+// note, property decorators execute during class declaration only!!!
+function Log(target: any, propertyName: string | Symbol) {
+	console.log("Property descriptor!");
+	console.log(target, propertyName);
+}
+
+// accessor decorator
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+	console.log("Accessor decorator!");
+	console.log(target);
+	console.log(name);
+	console.log(descriptor);
+}
+
+// method decorator
+function Log3(
+	target: any,
+	name: string | Symbol,
+	descriptor: PropertyDescriptor
+) {
+	console.log("Method decorator!");
+	console.log(target);
+	console.log(name);
+	console.log(descriptor);
+}
+
+// parameter decorator
+function Log4(target: any, name: string | Symbol, position: number) {
+	console.log("Method decorator!");
+	console.log(target);
+	console.log(name);
+	console.log(position);
+}
+
+class Product {
+	@Log
+	title: string;
+	private _price: number;
+
+	@Log2
+	set price(val: number) {
+		if (val > 0) {
+			this._price = val;
+		} else {
+			throw new Error("Invalid price - should be positive!");
+		}
+	}
+
+	constructor(t: string, p: number) {
+		this.title = t;
+		this._price = p;
+	}
+
+	@Log3
+	getPriceWithTax(@Log4 tax: number) {
+		return this._price * (1 + tax);
 	}
 }
