c85f2a19ee58f457ae66788755cb53991032ae93
cjmoro <scjmoro@gmail.com>
2020-04-05 16:13:59 -0700

    Add ProjectList class

--- drag-and-drop/app.ts
+++ drag-and-drop/app.ts
@@ -58,10 +58,10 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
 	return adjDescriptor;
 }
 
-// initial render logic
+// initial input render logic
 class ProjectInput {
 	templateElement: HTMLTemplateElement;
-	hotstElement: HTMLDivElement;
+	hostElement: HTMLDivElement;
 	element: HTMLFormElement;
 	titleInputElement: HTMLInputElement;
 	descriptionInputElement: HTMLInputElement;
@@ -71,7 +71,7 @@ class ProjectInput {
 		this.templateElement = document.querySelector(
 			"#project-input"
 		)! as HTMLTemplateElement;
-		this.hotstElement = document.querySelector("#app")! as HTMLDivElement;
+		this.hostElement = document.querySelector("#app")! as HTMLDivElement;
 
 		const importedNode = document.importNode(
 			this.templateElement.content,
@@ -99,7 +99,7 @@ class ProjectInput {
 	}
 
 	private attach() {
-		this.hotstElement.insertAdjacentElement("afterbegin", this.element);
+		this.hostElement.insertAdjacentElement("afterbegin", this.element);
 	}
 
 	@autobind
@@ -164,4 +164,41 @@ class ProjectInput {
 	}
 }
 
+// existing projects render logic
+class ProjectList {
+	templateElement: HTMLTemplateElement;
+	hostElement: HTMLDivElement;
+	element: HTMLElement;
+
+	constructor(private type: "active" | "finished") {
+		this.templateElement = document.querySelector(
+			"#project-list"
+		)! as HTMLTemplateElement;
+		this.hostElement = document.querySelector("#app")! as HTMLDivElement;
+
+		const importedNode = document.importNode(
+			this.templateElement.content,
+			true
+		);
+		this.element = importedNode.firstElementChild as HTMLElement;
+		this.element.id = `${this.type}-projects`;
+
+		this.attach();
+		this.renderContent();
+	}
+
+	private renderContent() {
+		const listId = `${this.type}-projects-list`;
+		this.element.querySelector("ul")!.id = listId;
+		this.element.querySelector("h2")!.textContent =
+			this.type.toUpperCase() + " PROJECTS";
+	}
+
+	private attach() {
+		this.hostElement.insertAdjacentElement("beforeend", this.element);
+	}
+}
+
 const prjInput = new ProjectInput();
+const activePrjList = new ProjectList("active");
+const finishedPrjList = new ProjectList("finished");
