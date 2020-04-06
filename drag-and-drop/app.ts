19a581263e899469c101fe5e96a4cb57fb039a7a
cjmoro <scjmoro@gmail.com>
2020-04-05 22:52:12 -0700

    Add ProjectItem class

--- drag-and-drop/app.ts
+++ drag-and-drop/app.ts
@@ -250,6 +250,29 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
 	}
 }
 
+// renders project items
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+	private project: Project;
+
+	constructor(typeId: string, project: Project) {
+		super("single-project", typeId, "afterbegin", project.id);
+		this.project = project;
+
+		this.configure();
+		this.renderContent();
+	}
+
+	configure() {}
+
+	renderContent() {
+		this.element.querySelector("h2")!.textContent = this.project.title;
+		this.element.querySelector(
+			"h3"
+		)!.textContent = this.project.people.toString();
+		this.element.querySelector("p")!.textContent = this.project.description;
+	}
+}
+
 // existing projects render logic
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
 	assignedProjects: Project[] = [];
@@ -288,9 +311,7 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
 		)! as HTMLUListElement;
 		listEl.innerHTML = "";
 		for (const prjItem of this.assignedProjects) {
-			const listItem = document.createElement("li");
-			listItem.textContent = prjItem.title;
-			listEl.appendChild(listItem);
+			new ProjectItem(this.element.querySelector("ul")!.id, prjItem);
 		}
 	}
 }
