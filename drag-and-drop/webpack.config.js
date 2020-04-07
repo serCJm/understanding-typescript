9b75ce64a8dd2c4d6facc0d4e0a4906c1fcf0cbf
cjmoro <scjmoro@gmail.com>
2020-04-07 16:30:17 -0700

    Add initial webpack configuration

---
+++ drag-and-drop/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+	entry: "./src/app.ts",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	devtool: "inline-source-map",
+	module: {
+		rules: [{ test: /\.ts$/, use: "ts-loader" }],
+	},
+	resolve: {
+		extensions: [".ts", ".js"],
+	},
+};
